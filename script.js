21d7bb427dfb500d5b786265a677f9fb70c3a491
gastonmieres <g4ston.m1eres@gmail.com>
2023-09-18 15:56:59 -0300

    Cambios en el estilo y funcionalidad

--- script.js
+++ script.js
@@ -1,10 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
     const getCountryInfoButton = document.getElementById("get-country-info");
+    const countryInfoDiv = document.getElementById("country-info");
+    const pageContainer = document.querySelector(".page-container");
 
     getCountryInfoButton.addEventListener("click", () => {
         const countryNameInput = document.getElementById("country-input").value;
 
         if (countryNameInput) {
+            // Aplicar transformación al "page-container" antes de hacer la solicitud
+            pageContainer.style.margin = "1rem auto";
+
             fetch(`https://restcountries.com/v3.1/name/${countryNameInput}?full=true`)
                 .then((response) => response.json())
                 .then((data) => {
@@ -15,10 +20,20 @@ document.addEventListener("DOMContentLoaded", () => {
                         const countryPopulation = countryData.population;
                         const countryArea = countryData.area;
 
+                        // Mostrar la información gradualmente
                         document.getElementById("country-name").textContent = countryName;
-                        document.getElementById("country-capital").textContent = countryCapital;
-                        document.getElementById("country-population").textContent = countryPopulation;
-                        document.getElementById("country-area").textContent = countryArea;
+                        setTimeout(() => {
+                            document.getElementById("country-capital").textContent = countryCapital;
+                        }, 2000); // Espera 1000ms (2 segundo) antes de mostrar el siguiente dato
+                        setTimeout(() => {
+                            document.getElementById("country-population").textContent = countryPopulation;
+                        }, 4000); // Espera 2000ms (4 segundos) antes de mostrar el siguiente dato
+                        setTimeout(() => {
+                            document.getElementById("country-area").textContent = countryArea;
+                        }, 6000); // Espera 3000ms (6 segundos) antes de mostrar el último dato
+
+                        // Mostrar la información del país con una transición suave
+                        countryInfoDiv.classList.remove("hidden");
                     } else {
                         alert("No se encontraron datos para ese país.");
                     }
